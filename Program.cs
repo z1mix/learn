a9840541f28d19ed51473ef2464156835b88c775
z1mix <3376115002@qq.com>
2026-06-03 15:17:32 +0800

    提交全部文件

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DecisionMaking
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 局部变量定义 */
+            int a = 10;
+
+            /* 使用 if 语句检查布尔条件 */
+            if (a < 20)
+            {
+                /* 如果条件为真，则输出下面的语句 */
+                Console.WriteLine("a 小于 20");
+            }
+            Console.WriteLine("a 的值是 {0}", a);
+            Console.ReadLine();
+        }
+    }
+}
